bfc982588fa9e534e6998668cf2fd93510d9f4f5
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2018-06-10 06:41:14 +0000

    Add files via upload

---
+++ fragments/year_of_passing.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<td class="sTD">
+    <label><span class="style2">*<span class="style10">Year of passing</span></span></label>
+</td>
+
+<td class="sTD">
+  <span id="yearList" style="width:400px;">
+      <select id="select2" name="select2" required>
+          <option value="" selected="selected">Select Year</option>
+           <option value="2016">2018</option>
+		  <option value="2016">2017</option>
+		  <option value="2016">2016</option>
+		  <option value="2015">2015</option>
+
+      </select>
+  </span>	  
+</td>
